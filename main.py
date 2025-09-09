0936a5c33e964467c3b890357410d1a0526f9719
Oleg Mat <mos_olma@mail.ru>
2025-09-09 17:17:22 +0300

    files changed: flake8, mypy, black, isort

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+lst = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+]
+
+for i in lst:
+    print(mask_account_card(i))
+
+# данные для проверки даты
+print("------------DATE--------------")
+print(get_date("2024-03-11T02:26:18.671407"))
