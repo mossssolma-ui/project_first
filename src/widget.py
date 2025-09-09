11ac20a24e3b47c393c44a4b413861e03aef4bda
Oleg Mat <mos_olma@mail.ru>
2025-09-09 17:04:39 +0300

    add func mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,30 @@
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(account_card: str) -> str:
+    """Функция обработки информации о карте и о счете"""
+    choice_acc = account_card.split()
+    if choice_acc[0] in 'Visa':
+        result = f"{choice_acc[0]} {choice_acc[1]} {get_mask_card_number(int(choice_acc[-1]))}"
+    elif choice_acc[0] in ['Maestro', 'MasterCard']:
+        result = f"{choice_acc[0]} {get_mask_card_number(int(choice_acc[-1]))}"
+    elif choice_acc[0] == 'Счет':
+        result = f"{choice_acc[0]} {get_mask_account(int(choice_acc[-1]))}"
+    else:
+        result = 'Данные отсутствуют'
+
+    return result
+
+
+# данные для проверки
+lst = ["Maestro 1596837868705199",
+       "Счет 64686473678894779589",
+       "MasterCard 7158300734726758",
+       "Счет 35383033474447895560",
+       "Visa Classic 6831982476737658",
+       "Visa Platinum 8990922113665229",
+       "Visa Gold 5999414228426353",
+       "Счет 73654108430135874305"]
+
+for i in lst:
+    print(mask_account_card(i))
